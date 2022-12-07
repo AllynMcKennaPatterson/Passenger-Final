1a24a0d5eec669595ec9c6194e45d025d1c3f90b
Allyn <G00392765@atu.ie>
2022-12-07 09:24:03 +0000

    Created passenger class with instance variables

---
+++ src/main/java/ie/atu/Passenger.java
@@ -0,0 +1,11 @@
+package ie.atu;
+
+public class Passenger {
+    private String passengerTitle;
+    private String passengerName;
+    private long passengerId;
+    private long passengerPhone;
+    private int passengerAge;
+    
+
+}
