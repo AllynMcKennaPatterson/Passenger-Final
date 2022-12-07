9716a779f07ab5ef1fbe3a71db4f4b9779936d6a
Allyn <G00392765@atu.ie>
2022-12-07 09:24:34 +0000

    Generated passenger constructor

--- src/main/java/ie/atu/Passenger.java
+++ src/main/java/ie/atu/Passenger.java
@@ -6,6 +6,12 @@ public class Passenger {
     private long passengerId;
     private long passengerPhone;
     private int passengerAge;
-    
 
+    public Passenger(String passengerTitle, String passengerName, long passengerId, long passengerPhone, int passengerAge) {
+        this.passengerTitle = passengerTitle;
+        this.passengerName = passengerName;
+        this.passengerId = passengerId;
+        this.passengerPhone = passengerPhone;
+        this.passengerAge = passengerAge;
+    }
 }
