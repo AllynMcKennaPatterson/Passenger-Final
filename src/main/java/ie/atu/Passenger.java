88e4500d32a9dee4069db588f7954c3cf89094f6
Allyn <G00392765@atu.ie>
2022-12-07 09:35:12 +0000

    Added setters with error checking

--- src/main/java/ie/atu/Passenger.java
+++ src/main/java/ie/atu/Passenger.java
@@ -34,4 +34,52 @@ public class Passenger {
     public int getPassengerAge() {
         return passengerAge;
     }
+
+    public void setPassengerTitle(String passengerTitle) {
+        if("Mr".equalsIgnoreCase(passengerTitle) || "Ms".equalsIgnoreCase(passengerTitle) || "Mrs".equalsIgnoreCase(passengerTitle)){
+            this.passengerTitle = passengerTitle;
+        }
+        else{
+            throw new IllegalArgumentException("Title must be Mr, Mrs, or Ms");
+        }
+
+    }
+
+    public void setPassengerName(String passengerName) {
+        if (passengerTitle.length() >= 3) {
+            this.passengerName = passengerName;
+        }
+        else{
+            throw new IllegalArgumentException("Name must have at least 3 characters");
+        }
+
+    }
+
+    public void setPassengerId(long passengerId) {
+        if(passengerId >= 0){
+            this.passengerId = passengerId;
+        }
+        else{
+            throw new IllegalArgumentException("Id must be a positive integer");
+        }
+    }
+
+    public void setPassengerPhone(long passengerPhone) {
+        if(String.valueOf(passengerPhone).length() == 8){
+            this.passengerPhone = passengerPhone;
+        }
+        else{
+            throw new IllegalArgumentException("Phone number must have 8 digits");
+        }
+    }
+
+    public void setPassengerAge(int passengerAge) {
+        if(passengerAge >= 16){
+            this.passengerAge = passengerAge;
+        }
+        else{
+            throw new IllegalArgumentException("Passenger must be at least 16 years old");
+        }
+
+    }
 }
